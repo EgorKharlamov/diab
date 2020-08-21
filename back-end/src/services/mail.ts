48b499479cc505e2e79bf7dd2d914e9f1b884ea3
EgorKharlamov <egorkharlamov9338@gmail.com>
2020-08-21 22:06:52 +0700

    mail class service

---
+++ back-end/src/services/mail.ts
@@ -0,0 +1,41 @@
+import nodemailer from 'nodemailer';
+import { verificationTokenLifeTimeMin } from '../helpers/tokensLife';
+
+export class Mailer {
+    transportOpt: object;
+
+    mailOpt: object;
+
+    constructor(targetMail:string) {
+      this.transportOpt = {
+        service: process.env.MAIL_SERVICE_NAME,
+        port: Number(process.env.MAIL_PORT),
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS,
+        },
+      };
+
+      this.mailOpt = {
+        from: process.env.MAIL_USER,
+        to: targetMail,
+      };
+    }
+
+    async sendVerification(token:string) {
+      const transporter = nodemailer.createTransport(this.transportOpt);
+
+      const mailOpt = {
+        ...this.mailOpt,
+        subject: 'Account verification',
+        text: `Hi there! Its your verification code, be careful!\n${token}\nYou have ${verificationTokenLifeTimeMin} min!`,
+      };
+
+      await transporter.sendMail(mailOpt, async (err) => {
+        if (err) {
+          return 'error';
+        }
+        return 'Success!';
+      });
+    }
+}
