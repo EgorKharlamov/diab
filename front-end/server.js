862e7308a4dbca03c9a0bb033bb684df8f9fd0e0
EgorKharlamov <egorkharlamov9338@gmail.com>
2020-08-22 22:10:27 +0700

    start, try async req

--- front-end/server.js
+++ front-end/server.js
@@ -1,14 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires,no-undef
 const express = require('express');
+// eslint-disable-next-line @typescript-eslint/no-var-requires,no-undef
 const path = require('path');
 const app = express();
 
+// eslint-disable-next-line no-undef
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
+  // eslint-disable-next-line no-undef
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 const startServer = async(p)=>{
   app.listen(p);
-}
+};
 
-const port = 9000
-startServer(port).then(()=>console.log(`front server started at port ${port}`))
+const port = 9000;
+startServer(port).then(()=>console.log(`front server started: http://localhost:${port}`));
--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,22 +1,35 @@
-import React, {useState, useEffect} from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {Route, Switch} from 'react-router-dom';
 import Home from './pages/Home';
 import List from './pages/List';
 import './App.css';
-
-import axios from 'axios';
+import {requester} from './helpers/requester';
 
 const App: React.FC = () => {
-  const [data, setData] = useState({res: []});
+  const [data, setData] = useState<string>();
 
+  useEffect(()=>{
+    const getUser = async() => {
+      return await requester(`{me 
+        {login 
+          {login pass email {value verified} phone {value}}
+        }
+      }`).then((result) => {
+        if (result.data.errors) {
+          return setData(result.data.errors[0].message);
+        }
+      });
+    };
+    getUser();
+  },[]);
 
-  
-  return (
+  return !data ? (<span>Loading...</span>) : (
     <div>
       <Switch>
         <Route exact path='/' component={Home}/>
         <Route path='/list' component={List}/>
       </Switch>
+      <div>{data}</div>
     </div>
   );
 };
